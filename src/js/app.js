47a0573a4c08ddb5b398651da1170ea3122c53e1
Ivan_Vierin <ivan@huncwot.com>
2022-08-11 22:38:17 +0200

    add some hover

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,10 @@ export class Page {
         window.addEventListener('load', () => {
             this.setComponents();
             new Animations();
+
+            if(window.ontouchstart === undefined) {
+                document.querySelector('html').classList.add('mod_no-touchevents');
+            }
         });
 
 
@@ -20,6 +24,37 @@ export class Page {
         //components
         new SwiperSlider();
         new SwiperMulti();
+        this.changeLogo()
+    }
+
+    changeLogo() {
+        const whites = [...document.querySelectorAll('.js-dark')].map(e => ({
+            top: e.getBoundingClientRect().top,
+            bottom: e.getBoundingClientRect().bottom
+        }));
+
+
+        // const blacks = [...document.querySelectorAll('.black')].map(e => ({top: e.top, bottom: e.bottom}));
+
+        const logo = document.querySelector('.js-logo');
+
+        document.addEventListener('scroll', () => {
+
+
+            let position = (logo.getBoundingClientRect().bottom + logo.getBoundingClientRect().top) / 2 + window.scrollY;
+
+            console.log(position);
+            for (let i = 0; i < whites.length; i++) {
+                if (position >= whites[i].top && position <= whites[i].bottom) {
+                    logo.classList.remove('blackLogo');
+                    logo.classList.add('whiteLogo');
+                    return;
+                }
+            }
+
+            logo.classList.remove('whiteLogo');
+            logo.classList.add('blackLogo');
+        });
     }
 
 }
