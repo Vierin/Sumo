9bd7988a111d55b34fc4058fe27661a1ab986686
Ivan_Vierin <ivan@huncwot.com>
2022-08-15 16:01:27 +0200

    fix date + start time

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -17,10 +17,43 @@ export class Form {
         this.inputs = this.view.querySelectorAll('input');
 
         this.inputs.forEach(inp => {
-            console.log(inp.value);
+            // console.log(inp.value);
         });
 
         // this.select();
+        this.picker = document.querySelector('#datepicker')
+        this.picker.querySelector('input').addEventListener('click', () => {
+            this.insertTime();
+            this.picker.classList.add('is-started');
+        });
+
+    }
+
+    insertTime() {
+        const box = document.querySelector('.datepicker-days table');
+
+        if(!this.picker.classList.contains('is-started')) {
+            box.insertAdjacentHTML('beforeend' , `
+                <div class="datepicker__time">
+                    <p>Time</p>
+                    <input class="time" type="time" name="time" value="00:00" required/>
+                    <div class="time-format">
+                        <input type="radio" id="time-am" name="time-format" value="am" checked/>
+                        <label for="time-am">AM</label>
+                        <input type="radio" id="time-pm" name="time-format" value="pm"/>
+                        <label for="time-pm">PM</label>
+                    </div>
+                </div>
+            `)
+        }
+
+
+
+
+        document.querySelectorAll('.time-format label')[1].addEventListener('click', () => {
+            // alert(123)
+        })
+        console.log(box);
     }
 
 
