342e89669d24adc46145bc008549f4b25569333c
Ivan_Vierin <ivan@huncwot.com>
2022-08-15 16:44:34 +0200

    fix time

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -30,36 +30,61 @@ export class Form {
     }
 
     insertTime() {
-        const box = document.querySelector('.datepicker-days table');
+        const box = document.querySelector('.datepicker-days');
 
         if(!this.picker.classList.contains('is-started')) {
             box.insertAdjacentHTML('beforeend' , `
                 <div class="datepicker__time">
                     <p>Time</p>
-                    <input class="time" type="time" name="time" value="00:00" required/>
+                    <input class="time" type="time" name="time" value="00:00" min="00:00" max="12:59" required/>
                     <div class="time-format">
-                        <input type="radio" id="time-am" name="time-format" value="am" checked/>
+                        <input type="radio" id="time-am" name="format" value="am" />
                         <label for="time-am">AM</label>
-                        <input type="radio" id="time-pm" name="time-format" value="pm"/>
+                        <input type="radio" id="time-pm" name="format" value="pm" checked/>
                         <label for="time-pm">PM</label>
                     </div>
                 </div>
             `)
         }
 
+        this.setTime();
+        this.changeTimeFormat();
 
+    }
 
-
-        document.querySelectorAll('.time-format label')[1].addEventListener('click', () => {
-            // alert(123)
+    setTime() {
+        const timeInputInside = this.view.querySelector('input[type=time]');
+        const timeInput = document.querySelector('.datepicker__time .time')
+        timeInput.addEventListener('click', () => {
+            setTimeout(() => {
+                timeInput.focus();
+            }, 10)
         })
-        console.log(box);
+
+        timeInput.addEventListener('input', () => {
+            timeInputInside.value = timeInput.value;
+
+        })
     }
 
 
-    // select() {
-    //     this.view.
-    // }
+    changeTimeFormat() {
+        const radios = document.querySelectorAll('.time-format label');
+
+        radios.forEach(el => {
+
+
+            el.addEventListener('click', () => {
+                const input = el.previousSibling.previousSibling;
+                input.checked = true;
+
+                console.log();
+                // const parent = el.parentElement
+                // parent.classList.remove('is-pm')
+                // parent.classList.add('')
+            })
+        });
+    }
 
 
 
